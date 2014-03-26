b3e5f5b2000821999a0e1a5bc4c379163614fd18
Daniel Burnham-King <drbking@gmail.com>
2014-03-26 15:37:45 +0200

    Added a keyGenerator class (code to still be implemented) - Started coding up the server...

--- src/fileSecurity/server/server.java
+++ src/fileSecurity/server/server.java
@@ -1,9 +1,35 @@
 package fileSecurity.server;
-
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.net.*;
 /**
  * @author Daniel Burnham-King
  * @author Merada Richter
  * 2014/03/26
  */
 public class server {
+
+    static ServerSocket socket;
+    protected final static int port = 8081;
+
+    public static void main(String args[])
+    {
+        try{
+            //create a new socket connection
+            socket = new ServerSocket(port);
+            p("Socket has been Initialized");
+
+
+        }
+        catch (IOException e){System.out.println("Error creating the socket on port:" + port);}
+    }
+
+    private static class ConHandler extends Thread{
+        private boolean authenticated = false;
+    }
+    //printHelper
+    static void p(String text)
+    {
+        System.out.println(text);
+    }
 }
