ad08f287a6269a0d542cf0663fd86c3a6bdb19a3
Daniel Burnham-King <drbking@gmail.com>
2014-03-26 17:41:54 +0200

    Added GSON, some work on client etc...

--- src/fileSecurity/client/client.java
+++ src/fileSecurity/client/client.java
@@ -1,9 +1,51 @@
 package fileSecurity.client;
+import com.google.gson.GsonBuilder;
+import fileSecurity.Handlers;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.*;
 
 /**
  * @author Daniel Burnham-King
  * @author Merada Richter
  * 2014/03/26
  */
-public class client {
+public class Client {
+    //Socket to handle the connection to the server
+    private Socket clientSocket;
+
+    private InputStreamReader input;
+    private OutputStreamWriter output;
+    Handlers.ResponseHandler reponse;
+    Handlers.RequestHandler request;
+
+    String SERVER = "localhost";
+    int PORT = 8087;
+
+
+    public static void main(String args[])
+    {
+
+    }
+
+    void connect ()
+    {
+
+        try
+        {
+            clientSocket = new Socket(InetAddress.getByName(SERVER),PORT);
+            System.out.println("Connected to "+SERVER + "at port:" + PORT);
+        }catch (IOException e){p("Error connecting with the server");}
+
+    }
+
+
+
+    void p(String text)
+    {
+        System.out.println(text);
+    }
+
 }
--- src/fileSecurity/server/server.java
+++ src/fileSecurity/server/server.java
@@ -1,16 +1,22 @@
 package fileSecurity.server;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.*;
+import java.security.*;
+
 /**
  * @author Daniel Burnham-King
  * @author Merada Richter
  * 2014/03/26
  */
-public class server {
+
+public class Server {
 
     static ServerSocket socket;
     protected final static int port = 8081;
+
 
     public static void main(String args[])
     {
@@ -22,11 +28,12 @@ public class server {
             while(true)
             {
                 ConHandler connection = new ConHandler(socket.accept());
+                p("Socket connection attempt made");
             }
-
         }
         catch (IOException e){System.out.println("Error creating the socket on port:" + port);}
     }
+
 
     private static class ConHandler{
         private boolean authenticated = false;
@@ -36,9 +43,9 @@ public class server {
         {
             this.socket = socket;
         }
-
     }
-    //printHelper
+
+    //Short print method
     static void p(String text)
     {
         System.out.println(text);
