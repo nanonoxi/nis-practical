05fc70d4b6adccb4b7bcefebe04d7085302039c7
Merada Richter <merada.richter@gmail.com>
2014-03-26 14:29:40 +0200

    Initial commit

---
+++ src/emailSecurity/client.java
@@ -0,0 +1,9 @@
+package emailSecurity;
+
+/**
+ * @author Daniel Burnham-King
+ * @author Merada Richter
+ * 2014/03/26
+ */
+public class client {
+}
---
+++ src/fileSecurity/client/client.java
@@ -0,0 +1,9 @@
+package fileSecurity.client;
+
+/**
+ * @author Daniel Burnham-King
+ * @author Merada Richter
+ * 2014/03/26
+ */
+public class client {
+}
