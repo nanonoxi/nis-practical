655a169fbd2c8462515708812e3d80cb34a3f7b4
Merada Richter <merada.richter@gmail.com>
2014-03-27 22:53:28 +0200

    Initial functionality

---
+++ src/emailSecurity/Config.java
@@ -0,0 +1,13 @@
+package emailSecurity;
+
+/**
+ * Created by meradarichter on 2014/03/27
+ */
+public class Config {
+
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String HOST = "smtp.gmail.com";
+    public static final String PORT = "587";
+
+}
--- src/emailSecurity/client.java
+++ src/emailSecurity/client.java
@@ -1,7 +1,33 @@
 package emailSecurity;
 
+import com.sun.mail.smtp.SMTPMessage;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.Security;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  * @author Daniel Burnham-King
@@ -10,28 +36,99 @@ import java.util.logging.Logger;
  */
 public class Client {
 
-    private static final Logger LOGGER = Logger.getLogger(Client.class.getName());
-
-
     static {
         Security.addProvider(new BouncyCastleProvider());
     }
 
     private void run() {
-        System.out.println("Hello world");
+        /*
+        Provide the email "essentials": SMTP server host & port, email addresses (sender & receiver), a subject, content, and the sending user's password
+        Add BC as a new crypto provider
+        Retrieve the cert from your Java Keystore
+        Create and sign the email using the BC API/libraries
+        Send the email
+         */
+
+        /** initialize session */
+        final String username = "nisuctassignment@gmail.com";
+        final String password = "nis@uct2014";
+
+        Properties props = new Properties();
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", Config.HOST);
+        props.put("mail.smtp.port", Config.PORT);
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        /** retrieve private key from p12 file */
+        String keyAlias = "key from secure.comodo.com";
+        String p12Password = "nis@uct2014";
+
+        KeyStore keystore = null;
+        try {
+            keystore = KeyStore.getInstance("PKCS12");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keystore.load(this.getClass().getClassLoader().getResourceAsStream("../../keys/niskey.p12"), p12Password.toCharArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+
+        PrivateKey key = null;
+        try {
+            key = (PrivateKey)keystore.getKey(keyAlias, p12Password.toCharArray());
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
+
+        /** sign email */
+        String emailContent = "Hello world";
+        String emailSubject = "Subject";
 
 
-        sign(email, key);
-        encrypt(email, key);
-        send(email);
+        // signing email
+        //Smsg = Sign(loginSession,msg) // Sign with certificate private key
+        /** encrypt email */
+
+        //Encrypt(loginSession, Smsg) // Encrypt with certificate pub key of toUser
+
+        /** send email */
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("nisuctassignment@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("nisuctassignment+dan@gmail.com"));
+            message.setSubject(emailSubject);
+            message.setText(emailContent);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    private void sign(String email, String key) {
-
-    }
-
 
     public static void main (String args []) {
-        new fileSecurity.client.Client().run();
+        (new Client()).run();
     }
 }
